090a72335d9be9c49ff1664cd7e62d97db6f7126
doctea <doctea@gmail.com>
2023-05-17 23:00:57 +0100

    protect from potential problem with accessing too many things

--- src/sequencer/Sequencer.cpp
+++ src/sequencer/Sequencer.cpp
@@ -1,9 +1,17 @@
 #include "sequencer/Sequencer.h"
 #include "sequencer/Patterns.h"
 
+#include "debug.h"
+
 //#include "outputs/output.h"
 
 void BaseSequencer::configure_pattern_output(int index, BaseOutput *output) {
+    if (index >= this->number_patterns) {
+        char message[MAX_MESSAGE_LOG*2];
+        snprintf(message, MAX_MESSAGE_LOG*2, "Attempted to configure pattern with invalid index %i!", index);
+        messages_log_add(message);
+        return;
+    }
     SimplePattern *p = this->get_pattern(index);
     p->set_output(output);
 }
